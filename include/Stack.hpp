519cc1c296e9558990b0a7fbefd9e599992f20d3
OzoNeTT <gpoff12@mail.ru>
2019-09-13 17:21:51 +0300

    Task2 + unit_tests

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <type_traits>
 #include <exception>
-#include <algorithm> //мало ли понадобится
+#include <utility>
 
 template <typename T>
 class Stack
 {
 private:
 struct Node {
-    T Data;
+    T _data;
     Node *next;
 };
 Node *_head;
@@ -19,22 +19,42 @@ public:
         _head = nullptr;
     }
     Stack(const Stack&) = delete;
+    Stack(Stack &&stack) noexcept = default;
+
     Stack& operator=(const Stack&) = delete;
+
     void push(T&& value){
-        Node *newNode = new Node{value, _head};
+        Node *newNode = new Node{std::forward<T>(value), _head};
         _head = newNode;
     }
     void push(const T& value){
         Node *newNode = new Node{value, _head};
         _head = newNode;
     }
-    void pop(){
-        _head = _head->next;
-    }
+
+    //void pop(){
+    //    if(!_head)
+    //        throw std::exception();
+    //    _head = _head->next;
+    //}
+
     const T& head() const{
         if(!_head)
             throw std::exception();
-        return _head->Data;
+        return _head->_data;
+    }
+    template <typename ... Args>
+    void push_emplace(Args&&... value){
+        auto *_next = _head;
+        _head = new Node{std::forward<Args>(value)..., _next};
+    }
+
+    T pop()  {
+        if(!_head)
+            throw std::exception();
+        T data = _head->_data;
+        _head = _head->next;
+        return data;
     }
     ~Stack()
     {
--- tests/test.cpp
+++ tests/test.cpp
@@ -7,11 +7,22 @@
 TEST(Stack, simple)
 {
     Stack<int> st;
-    st.push(14);
-    EXPECT_EQ(st.head(), 14);
-    st.push(11);
-    EXPECT_EQ(st.head(), 11);
+    st.push(10);
+    EXPECT_EQ(st.head(), 10);
+    st.push_emplace(1);
+    st.push_emplace(2);
+    st.push_emplace(3);
+    EXPECT_EQ(st.head(), 3);
+    st.push_emplace(4);
+    EXPECT_EQ(st.head(), 4);
+    st.push_emplace(5);
+    EXPECT_EQ(st.head(), 5);
     st.pop();
-    EXPECT_EQ(st.head(), 14);
+    EXPECT_EQ(st.head(), 4);
+    st.pop();
+    EXPECT_EQ(st.head(), 3);
+    st.pop();
+    EXPECT_EQ(st.head(), 2);
 
 }
+
